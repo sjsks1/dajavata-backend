1e4ecfc80e2ae25bab11006d2653866b9d020b3a
sjsks1 <jata8011@gmail.com>
2026-08-31 18:36:56 +0900

    Delete test_fdr_desc.py

--- test_fdr_desc.py
+++
@@ -1,8 +0,0 @@
-import FinanceDataReader as fdr
-
-try:
-    df_desc = fdr.StockListing('KRX-DESC')
-    print(df_desc.columns)
-    print(df_desc.head(2))
-except Exception as e:
-    print(f"Error: {e}")
